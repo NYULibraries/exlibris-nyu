896390ecfdb4d0a0e4e7a6b09a64e44261113f59
Scot Dalton <scotdalton@gmail.com>
2013-03-07 10:03:28 -0500

    Add Rails specific configuration if we're in the Rails environment.

--- lib/exlibris-primo-nyu.rb
+++ lib/exlibris-primo-nyu.rb
@@ -10,6 +10,14 @@ Exlibris::Aleph.configure do |config|
   config.load_yaml File.expand_path("#{File.dirname(__FILE__)}/../config/aleph.yml",  __FILE__)
 end
 
+# Load Rails specific configuration if we're in a Rails environment
+if defined?(::Rails) && ::Rails.version >= '3.1.0'
+  Exlibris::Aleph.configure do |config|
+    config.logger = Rails.logger
+    config.yml_path = "#{Rails.root}/config/aleph"
+  end
+end
+
 require "require_all"
 require_all "#{File.dirname(__FILE__)}/exlibris/"
 
