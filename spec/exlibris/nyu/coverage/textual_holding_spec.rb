ab4f718129b5dec9a77f40bcb2e23f15a64ad168
Scot Dalton <scotdalton@gmail.com>
2014-06-09 17:05:54 -0400

    Records no longer take an AdminLibrary so need to update the TextualHolding spec

--- spec/exlibris/nyu/coverage/textual_holding_spec.rb
+++ spec/exlibris/nyu/coverage/textual_holding_spec.rb
@@ -8,11 +8,11 @@ module Exlibris
         subject(:textual_holding) { TextualHolding.new(collection, value) }
         it { should be_a TextualHolding }
         describe '.from_bib_866', vcr: { cassette_name: "vogue" } do
-          let(:bib_library) { "NYU01" }
-          let(:record_id) { "002893728" }
-          let(:record) { Exlibris::Aleph::Record.new(bib_library: bib_library, record_id: record_id) }
-          let(:marc_bib) { record.bib }
-          let(:bib_866) { marc_bib.find { |field| field.tag == '866' } }
+          let(:record_id) { 'NYU01002893728' }
+          let(:record) { Exlibris::Aleph::Record.new(record_id) }
+          let(:record_metadata) { record.metadata }
+          let(:marc_record) { record_metadata.marc_record }
+          let(:bib_866) { marc_record.find { |field| field.tag == '866' } }
           subject { TextualHolding.from_bib_866(collection, bib_866) }
           it { should be_a TextualHolding }
         end
